df86dbde9a7bf14b9ea9604e7022d22dc7d02afa
iparenta <128072730+iparenta@users.noreply.github.com>
2023-04-21 11:05:33 +0200

    Add files via upload

---
+++ Vezba 11 zaglavlja/tv.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+void tvTurnOn();
+void tvTurnOff();
+void tvChannelUp();
+void tvChannelDown();
+unsigned char tvGetStatus();
+unsigned int tvGetCurrentChannel();
+void tvPrintInformation();
+
