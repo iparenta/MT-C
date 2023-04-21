54f513e0a9106e92ddf2c283c41c9cecc9a2e46b
iparenta <128072730+iparenta@users.noreply.github.com>
2023-04-21 11:06:16 +0200

    Create program.c

---
+++ Vezba 12 naredbe ponavljanja WHILE/program.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void printMenu(){
+    printf("-----------------------------\n");
+    printf("1 - Add new item\n");
+    printf("2 - Edit existing item\n");
+    printf("3 - Delete an item\n");
+    printf("0 - Exit program\n");
+
+}
+
+
+int askForChoice(){
+    int choice;
+    printf("Enter your choice:");
+    scanf("%d", &choice);
+    return choice;
+}
+
+
+int getChoiceFromMenu(){
+    int izbor = -1;
+
+    while(izbor < 0 || izbor > 3){
+        printMenu();
+        izbor = askForChoice();
+    }
+    return izbor;
+}
+
+
+
+
+int main(){
+
+    int izbor = getChoiceFromMenu();
+    printf("Korisnik je izabrao opciju %d.\n", izbor);
+
+
+}
